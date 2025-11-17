76f2492eca0cc7fcfe4b13f71a2f0068e4a816b0
josef <josef.rdz666@gmail.com>
2025-11-16 21:05:15 -0600

    Se actualizo el archivo Holagit.py

--- HolaGit.py
+++ HolaGit.py
@@ -1,9 +1,10 @@
 print("Hola mundo, estoy repasando git")
 
 
-#iniciar una carpeta para trabajar con git, para trabajar con un sistema de 
-#control de versiones: git init
+# iniciar una carpeta para trabajar con git, para trabajar con un sistema de
+# control de versiones: git init
 
 
-#la rama master/main es la principal del proyecto
+# la rama master/main es la principal del proyecto
 
+print("Nuevo texto!!!!")
