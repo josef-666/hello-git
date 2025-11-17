1b5e3fac9f6c429da1c0e59ccbc3f1afe74b2beb
josef <josef.rdz666@gmail.com>
2025-11-16 20:44:40 -0600

    Este es mi primer commit del file HolaGit.py

---
+++ HolaGit.py
@@ -0,0 +1,9 @@
+print("Hola mundo, estoy repasando git")
+
+
+#iniciar una carpeta para trabajar con git, para trabajar con un sistema de 
+#control de versiones: git init
+
+
+#la rama master/main es la principal del proyecto
+
